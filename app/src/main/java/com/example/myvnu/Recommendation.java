293c09049ba45c4d10979a15a6e063f5e00ad99f
trungtruc2357 <69811172+trungtruc2357@users.noreply.github.com>
2022-02-06 10:18:45 +0700

    add recomendation

---
+++ app/src/main/java/com/example/myvnu/Recommendation.java
@@ -0,0 +1,37 @@
+package com.example.myvnu;
+
+import android.content.Context;
+
+import com.example.myvnu.roomdatabase.Place;
+import com.example.myvnu.DBAction;
+
+import java.util.List;
+
+public class Recommendation {
+    private DBAction dbAction;
+
+    public List<Place> findPlaceWithQuery(Context context, String query){
+        String[] words = query.split(" ");
+        int numWords = 10;
+        if(words.length < 10) numWords = words.length;
+        while (numWords >= 0){
+            for (int i = 0; i + numWords < words.length; i++) {
+                String tag = words[i];
+                for (int j = 1; j < numWords; j++){
+                    tag = tag + " " + words[i+j];
+                }
+
+                List<Place> places = dbAction.findPlaceWithTag(context, tag);
+
+                if(places.size() > 0){
+                    return places;
+                }
+            }
+            numWords--;
+        }
+        return null;
+    }
+    public String makeIntro(Place place){
+        return place.getIntro();
+    }
+}
